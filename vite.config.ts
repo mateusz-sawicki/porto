09e80df0934c510fbdd8e95309588e611ba7eaf9
Mateusz <mathewsawicki@gmail.com>
2025-07-24 01:04:23 +0200

    dev env fix

--- vite.config.ts
+++ vite.config.ts
@@ -36,4 +36,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'porto.swck.pl',
+      'localhost',
+      '127.0.0.1',
+      '0.0.0.0'
+    ]
+  },
 })
